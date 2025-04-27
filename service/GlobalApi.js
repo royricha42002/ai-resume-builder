96ba03ff59fc7d02ac003c47bd0d17dab35c5608
Richa Roy <royricha42002@gmail.com>
2025-04-27 13:30:20 +0530

    3rd commit

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -2,8 +2,16 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 
+// const axiosClient = axios.create({
+//     baseURL: 'http://localhost:1337/api/',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': `Bearer ${API_KEY}`
+//     }
+// })
+
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:1337/api/',
+    baseURL: import.meta.env.VITE_BASE_URL+"/api/",
     headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${API_KEY}`
@@ -19,7 +27,7 @@ const UpdateResumeDetail = (id, data) => axiosClient.put('/user-resumes/' + id, 
 const GetResumeById = (id) => axiosClient.get('/user-resumes/' + id + "?populate=*")
 
 const DeleteResumeById = (id) => axiosClient.delete('/user-resumes/' + id)
-
+4
 export default {
     CreateNewResume,
     GetUserResumes,
